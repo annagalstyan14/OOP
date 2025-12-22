55d53c28520fcd1700c77805d819ac38b5871035
Anna Galstyan <anulgal04@gmail.com>
2025-12-22 12:42:47 +0400

    final changes

--- Action/AddShapeAction.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADD_SHAPE_ACTION_H_
-#define ADD_SHAPE_ACTION_H_
-
-// Wrapper header - includes the combined ShapeActions.h
-#include "ShapeActions.h"
-
-#endif // ADD_SHAPE_ACTION_H_
--- Action/AddSlideAction.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADD_SLIDE_ACTION_H_
-#define ADD_SLIDE_ACTION_H_
-
-// Wrapper header - includes the combined SlideActions.h
-#include "SlideActions.h"
-
-#endif // ADD_SLIDE_ACTION_H_
